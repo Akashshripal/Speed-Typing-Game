aac794f7a0ed5910b8a1e45401f2e60fccc64f38
Akashshripal <98543501+Akashshripal@users.noreply.github.com>
2023-01-23 14:59:26 +0530

    Update useWordGame.js

--- hooks/useWordGame.js
+++ hooks/useWordGame.js
@@ -1,6 +1,6 @@
 import {useState, useEffect, useRef} from "react"
 
-function useWordGame(startingTime = 10) {
+function useWordGame(startingTime = 30) {
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(startingTime)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
